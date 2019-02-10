4f1739bf9394aa2deb92e69796593ef7adf7ab9c
Benjamin <benman2005@gMAIL.com>
2019-02-10 13:35:10 +0100

    removed consolelog

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -8,7 +8,6 @@ export default class DetailsPage extends PureComponent {
     if(!this.props.location.state)return<Redirect to="/" />
     const {brouwerij, coords} = this.props.location.state;
     const beers = bieren.beers.filter(beer=>beer.brewery===brouwerij.name)
-    console.log(beers)
     return (
       <div className="details">
         <h1>{brouwerij.name}</h1>
